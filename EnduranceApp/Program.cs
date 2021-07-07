a29deb6c4db48150d049f5d6143dff9bdd53740c
Mark Francisco <mfrancisco581@gmail.com>
2021-07-07 12:50:26 -0500

    initial commit

---
+++ EnduranceApp/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EnduranceApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("   /|");
+            Console.WriteLine("  / |");
+            Console.WriteLine(" /  |");
+            Console.WriteLine("/___|");
+            Console.ReadLine();
+        }
+    }
+}
