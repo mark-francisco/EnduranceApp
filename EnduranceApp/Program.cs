96538ce8fdc8b2bcdc2080f0dd8a4df933e51e1e
Mark Francisco <mfrancisco581@gmail.com>
2021-07-09 01:25:13 -0500

    add more pseudocode

--- EnduranceApp/Program.cs
+++ EnduranceApp/Program.cs
@@ -13,6 +13,9 @@ namespace EnduranceApp
         static void Main(string[] args)
         {
             //TODO: figure out how to get a more dynamic filepath...
+            //TODO: split out functionality into separate methods, classes, components
+            //TODO: allow the User to pass in multiple mazes as arguments at once
+            //TODO: re-factor code and work on improving efficiency
             string filepath = @$"/Users/juanmarcofrancisco/Actualize/VisualStudioProjects/EnduranceApp/ExampleMazes/{args[0]}";
             Console.WriteLine(filepath);
             List<string> rows = File.ReadLines(filepath).ToList();
@@ -48,38 +51,45 @@ namespace EnduranceApp
             }
             //Console.WriteLine($"number of rows in maze: {numRows}");
             //Console.WriteLine($"number of cols in maze: {numCols}");
-            foreach (Cell cell in allCells)
-            {
-                Console.WriteLine("Cell:");
-                Console.WriteLine("Value: " + cell.Value);
-                Console.WriteLine("Y: " + cell.Y);
-                Console.WriteLine("X: " + cell.X);
-            }
 
-            Console.ReadLine();
-        }
-
-        static void SolveMaze(string[] args)
-        {
-
-            //www.techiedelight.com/find-shortest-path-in-maze/
-            //Classes: cells: value, xCoord, yCoord. currentCell: value, xCoord, yCoord.  
-
-            //allCells = matrix
-            //allCells.each { |cell|
-            //  if cell == 1
+            //foreach (Cell cell in allCells)
+            //{
+            //    Console.WriteLine("Cell:");
+            //    Console.WriteLine("Value: " + cell.Value);
+            //    Console.WriteLine("Y: " + cell.Y);
+            //    Console.WriteLine("X: " + cell.X);
             //}
 
-            //there is only one legitimate path. approach: try traversing each possible route as far as you can. see if you can hit the end. if not, backtrack as necessary.
 
-            //nodes = []. iterate thru matrix. use the indexes to put all the coordinates of cells with 1's in an array.
-            //the 1s are the nodes of a tree. start with the first cell in the array (the one with a y-coordinate of 1). 
-            //checked = [] (array of arrays). var to represent which cells/nodes have already been checked.
-            //the index of the node in nodes is the node #.
+
+            //www.techiedelight.com/find-shortest-path-in-maze/
+            //Assumption: there is only one legitimate path.
+            //Approach: traverse thru each possible route as far as you can. see if you can hit the end. if not, backtrack as necessary.
+
+
+            //filter out only the cells that are 1's.
+            //nodes = []. iterate thru allCells. put all the Cells with 1's in an array.
+            //solutionCell = nodes[nodes.Count() - 1];
+
+            //checked = [] (array of cells). initialize this var to represent which cells/nodes have already been checked.
+
+
+            //start with the first cell in the array (the one with a y-coordinate of 1). nodes[0].
+
+            //use the Cell's getter methods (Cell.X and Cell.Y) to search thru each Cell in allCells. then at the end, map the Cell objects into [y,x] array items. 
+
+
+
+
+
             //the edge is whether or not an adjacent cell also exists in nodes and has not been checked yet. (check if the theoretical adjacent targetCell's coordinates exists in nodes AND hasn't been checked yet. if so, it's a valid edge/move.)
-            //write a function that loops thru the maze. start on line 1 and end at line n. stop once you've hit a 1 on the last row of the file.
+
+
+            //write a function that loops thru the maze (each cell in nodes). first, check if the current node is the solutionCell (if so, then populate the path). then, add that node to the checked array. we are checking whether that node is allowed to make a legitimate move. if so, re-run the function with the next node that was moved to (find that Cell in the nodes array). if not, break out of the function call and go back up one level to the previous fn call (go back 1 level in the stack).
+
+            //start on line 1 and end at line n. stop once you've hit a 1 on the last row of the file.
             //currentCell = [y,x]
-            //solutionCell = [y,x]
+
             //path = [] (array of arrays)
 
             //define the operations/possible moves: down, left, up, right
@@ -89,8 +99,16 @@ namespace EnduranceApp
             // once you've found a cell that lets you keep going/lets you make the move, recursively call the function again, but with the targetCell as the new currentCell. add targetCell to checked array.
             //if you can't keep going with a valid move and need to backtrack, backtrack 1 cell/function call. 
             //try to run the other directions that haven't been checked yet to try and get a possible move. backtrack further (return false and go back up 1 recursive fn call in the call stack) whenever the current cell you're on has no more possible options/moves.
-            //if you reach the solution cell, then return true and recursively go back up the fn calls in the call stack. add the currentCell's coordinates to "path" each time. do a .Push to the stack every time the function is recursively run (at the end of the fn, after the recursive fn calls).
+            //if you reach the solution cell, then return true and recursively go back up the fn calls in the call stack. add the currentCell's coordinates to "path" each time.
 
+
+
+            Console.ReadLine();
         }
+
+        static void SolveMaze()
+        {}
+        static void ValidateCellNotChecked() { }
+        static void ValidateValidMove() { }
     }
 }
