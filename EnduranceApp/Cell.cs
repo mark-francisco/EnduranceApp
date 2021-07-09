c3b860c6bc45434e578e16f00ddaeeae8ff3aae0
Mark Francisco <mfrancisco581@gmail.com>
2021-07-08 23:25:48 -0500

    try to figure out parsing from txt files in C#

---
+++ EnduranceApp/Cell.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace EnduranceApp
+{
+    public class Cell
+    {
+        public int Value { get; set; }
+        public int YCoord { get; set; }
+        public int XCoord { get; set; }
+    }
+}
--- EnduranceApp/Program.cs
+++ EnduranceApp/Program.cs
@@ -1,33 +1,68 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace EnduranceApp
 {
     class Program
     {
-        static void Main(string[] matrix)
+        // instructions:
+        // user must change filepath to represent the directory on their machine on which the application lives
+        // user must pass in .txt files to the console app as fn arguments
+        static void Main(string[] args)
         {
-            // www.techiedelight.com/find-shortest-path-in-maze/
-            //intake a .txt file as an argument
-            //question: how to parse thru txt files and find out how many rows and columns it has.
-            //question: how to tell the value of a cell: if it's 1 or 0?
-            string[] lines = File.ReadAllLines(@"/Users/juanmarcofrancisco/Actualize/VisualStudioProjects/EnduranceApp/maze1.txt");
-            var allCells = new List<string>();
-            Console.WriteLine(allCells);
+            string filepath = @$"/Users/juanmarcofrancisco/Actualize/VisualStudioProjects/EnduranceApp/ExampleMazes/{args[0]}";
+            Console.WriteLine(filepath);
+            List<string> rows = File.ReadLines(filepath).ToList();
+            Console.WriteLine(rows);
 
-            foreach (string line in lines)
-            Console.WriteLine(allCells);
-            //allCells.Add(line);
+            int i = 0;
+            foreach (string row in rows)
+            {
+                int rowNum = i + 1;
+                Console.WriteLine("row " + rowNum + ": " + row);
+                string[] cells = row.Split(" ");
+                Console.Write(cells);
+
+                //foreach (char cell in row)
+                //{
+                //    //List<string> cells = row.Split(" ");
+                //    Console.Write(cell + ", ");
+                //}
+
+                i++;
+            }
+
+            //foreach (string line in lines)
+            //{
+            //    Console.WriteLine(line);
+            //    string[] cells = line.Split(" ");
+
+            //    Cell newRow = new Cell();
+            //}
             
+            //Console.ReadLine();
+        }
+
+        static void SolveMaze(string[] args)
+        {
+            
+            //www.techiedelight.com/find-shortest-path-in-maze/
+            //question: how to parse thru txt files and find out how many rows and columns it has?
+            //create a matrix of values (1's and 0's) from the .txt file. use the outer and inner indexes to get the y and x coordinates.
+            //numRows = outer index + 1.
+
+            //Classes: Maze: numRows,numCols. cells: value, xCoord, yCoord. currentCell: value, xCoord, yCoord.  
+
             //allCells = matrix
             //allCells.each { |cell|
             //  if cell == 1
             //}
 
-            //there is only one legitimate path. so, brute force approach: try traversing each possible route as far as you can. see if you can hit the end. if not, backtrack as necessary.
+            //there is only one legitimate path. approach: try traversing each possible route as far as you can. see if you can hit the end. if not, backtrack as necessary.
 
-            //nodes = []. iterate thru text file. put all the coordinates of cells with 1's in an array.
+            //nodes = []. iterate thru matrix. use the indexes to put all the coordinates of cells with 1's in an array.
             //the 1s are the nodes of a tree. start with the first cell in the array (the one with a y-coordinate of 1). 
             //checked = [] (array of arrays). var to represent which cells/nodes have already been checked.
             //the index of the node in nodes is the node #.
@@ -36,7 +71,6 @@ namespace EnduranceApp
             //currentCell = [y,x]
             //solutionCell = [y,x]
             //path = [] (array of arrays)
-            //Stack<Array> stack = new Stack<Array>();
 
             //define the operations/possible moves: down, left, up, right
             //define the neighbors. down = (y + 1, x), left = (y, x - 1), up = (y - 1, x), right = (y, x + 1)
@@ -47,10 +81,6 @@ namespace EnduranceApp
             //try to run the other directions that haven't been checked yet to try and get a possible move. backtrack further (return false and go back up 1 recursive fn call in the call stack) whenever the current cell you're on has no more possible options/moves.
             //if you reach the solution cell, then return true and recursively go back up the fn calls in the call stack. add the currentCell's coordinates to "path" each time. do a .Push to the stack every time the function is recursively run (at the end of the fn, after the recursive fn calls).
 
-
-
-            //Objects: Maze: numRows,numCols. currentCell: value, xCoord, yCoord.  
-            
         }
     }
 }
